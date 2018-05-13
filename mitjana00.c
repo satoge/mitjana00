40d60b8746ef4ff5f788f99aae225625c02af6ac
satoge <satoge1718@gmail.com>
2018-05-13 22:55:44 +0200

    conflicte entre master i br0

--- mitjana00.c
+++ mitjana00.c
@@ -21,6 +21,7 @@ int main() {
 		scanf(" %c",&consulta); 
 	}
 	mitjana=suma/numero_repeticions;
-	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("La mitjana resultant és: %.2f\n",mitjana);
+	printf("La mitjana ha estat trobada amb exit");	
 	return 0;
 }
